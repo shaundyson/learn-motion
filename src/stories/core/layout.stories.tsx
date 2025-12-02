9a36c92f9b28a043fa6d588cdaf99cda223064c7
shaun <shaundyson2022@gmail.com>
2025-12-02 19:09:42 +0000

    feat: add layout story with Switch component and Description integration

---
+++ src/stories/core/layout.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import Description from "../components/Description";
+import * as motion from "motion/react-client";
+import { useState } from "react";
+import clsx from "clsx";
+
+const meta: Meta = {
+  tags: ["autodocs"],
+  decorators: [
+    (Story, { parameters }) => (
+      <div className="flex flex-col h-screen w-screen items-center justify-center ">
+        {parameters.description ? (
+          <Description description={parameters.description} />
+        ) : null}
+        <div className="flex-1 flex items-center justify-center">
+          <Story />
+        </div>
+      </div>
+    ),
+  ],
+  parameters: {
+    layout: "centered",
+  },
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Switch: Story = {
+  render: () => {
+    const [isOn, setIsOn] = useState(false);
+    const toggleSwitch = () => setIsOn(!isOn);
+    return (
+      <div
+        onClick={toggleSwitch}
+        className={clsx("w-20 h-10 flex border-2 rounded-full p-2", {
+          "justify-end": isOn,
+        })}
+      >
+        <motion.div className="bg-green-500 rounded-full w-6" layout />
+      </div>
+    );
+  },
+};
