64ee9402909b624437d073674f586c20c97d15eb
shaun <shaundyson2022@gmail.com>
2025-12-02 21:21:54 +0000

    fix: remove unused import of ComponentProps in layout stories

--- src/stories/core/layout.stories.tsx
+++ src/stories/core/layout.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import Description from "../components/Description";
 import * as motion from "motion/react-client";
-import { useState, type ComponentProps } from "react";
+import { useState } from "react";
 import { cn } from "@/lib/utils";
 
 const meta: Meta = {
